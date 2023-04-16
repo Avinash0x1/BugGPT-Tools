567f1723a4b9e996aaf1c372183be9e3413d17f6
Ajam <AjamX101@gmail.com>
2023-04-16 14:56:06 +0545

    update now shows more info

--- linky/linky.sh
+++ linky/linky.sh
@@ -77,7 +77,7 @@ if [[ $# -gt 0 && ( "$*" == *"up"* || "$*" == *"-up"* || "$*" == *"update"* || "
              dos2unix $REMOTE_FILE > /dev/null 2>&1 
              sudo mv "$REMOTE_FILE" /usr/local/bin/linky && echo -e "➼ ${GREEN}Updated${NC} to ${BLUE}@latest${NC}\n" 
              echo -e "➼ ${YELLOW}ChangeLog:${NC} ${PINK}$(curl -s https://api.github.com/repos/Azathothas/BugGPT-Tools/commits?path=linky/linky.sh | jq -r '.[0].commit.message')${NC}"
-             echo -e "${YELLOW}Pushed at${NC}: ${BLUE}$(curl -s https://api.github.com/repos/Azathothas/BugGPT-Tools/commits?path=linky/linky.sh | jq -r '.[0].commit.author.date')${NC}\n"
+             echo -e "➼ ${YELLOW}Pushed at${NC}: ${BLUE}$(curl -s https://api.github.com/repos/Azathothas/BugGPT-Tools/commits?path=linky/linky.sh | jq -r '.[0].commit.author.date')${NC}\n"
              sudo chmod +xwr /usr/local/bin/linky
              rm -f "$REMOTE_FILE" 2>/dev/null
              else
