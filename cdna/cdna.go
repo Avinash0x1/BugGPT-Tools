f6c3e221950ea6a6db1c592dd41553faaf66a960
Ajam <AjamX101@gmail.com>
2023-04-17 12:18:22 +0545

    v0.0.5

--- cdna/cdna.go
+++ cdna/cdna.go
@@ -2,40 +2,135 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/projectdiscovery/cdncheck"
 )
 
 func main() {
+	//flags
+	inputPtr := flag.String("i", "", "path to input file containing IP addresses")
+	noCDNPtr := flag.Bool("n", false, "only print IPs that don't have a CDN")
+	noColorPtr := flag.Bool("sc", false, "suppress output colors")
+	flag.Usage = func() {
+		var usage = `Options:
+  -i    path to input file containing IP addresses
+  -n    only print IPs that don't have a CDN
+  -sc   suppress all output colors
+
+Examples:
+
+Quickly get IP:CDN          :: ➼ cdna -i ip-addr.txt 	
+Or via stdin                :: ➼ cat ip-addr.txt | cdna
+Print only ips with no cdns :: ➼ cat ip-addr.txt | cdna -n
+More: https://github.com/Azathothas/BugGPT-Tools/tree/main/cdna
+`	
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, usage)
+	}
+	
+	flag.Parse()
+    // Colors
+	blue := color.New(color.FgBlue).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	orange := color.New(color.FgHiYellow).SprintFunc()
+    //check for updates
+	updates.Check("Azathothas", "BugGPT-Tools", "cdna")
+	//main functions
+	var scanner *bufio.Scanner
+	if *inputPtr != "" {
+		file, err := os.Open(*inputPtr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(file)
+	} else {
+		scanner = bufio.NewScanner(os.Stdin)
+	}
+
 	// uses projectdiscovery endpoint with cached data to avoid ip ban
 	// Use cdncheck.New() if you want to scrape each endpoint (don't do it too often or your ip can be blocked)
 	client, err := cdncheck.NewWithCache()
 	if err != nil {
-		log.Fatal(err)
+		if !*noColorPtr {
+			color.Red("! Error contacting cdn.nuclei.sh")
+			color.Red("! Either the cdn provider is down or your internet is")
+		} else {
+			fmt.Println("! Error contacting cdn.nuclei.sh")
+			fmt.Println("! Either the cdn provider is down or your internet is")
+		}
+		return
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		ipStr := scanner.Text()
 		ip := net.ParseIP(ipStr)
 		if ip == nil {
-			fmt.Printf("%s is not a valid IP address\n", ipStr)
+			if !*noColorPtr {
+				color.Red("%s is not a valid IP address", ipStr)
+			} else {
+				fmt.Printf("%s is not a valid IP address\n", ipStr)
+			}
 			continue
 		}
 		found, result, err := client.Check(ip)
 		if found && err == nil {
-			fmt.Printf("%s\n", result)
+			if *noCDNPtr {
+				if result == "" {
+					if !*noColorPtr {
+						color.Green("%s", ipStr)
+					} else {
+						fmt.Println(ipStr)
+					}
+				}
+				continue
+			}
+			if result != "" {
+				if !*noColorPtr {
+					fmt.Printf("%s:%v\n", blue(ipStr), orange(result))
+				} else {
+					fmt.Printf("%s:%v\n", ipStr, result)
+				}
+			} else {
+				if !*noColorPtr {
+					color.Blue("%s", ipStr)
+				} else {
+					fmt.Println(ipStr)
+				}
+			}
 		} else if err != nil {
-			fmt.Printf("%s: error checking CDN: %v\n", ipStr, err)
+			if !*noColorPtr {
+				color.Red("! Error contacting cdn.nuclei.sh")
+			} else {
+				fmt.Println("! Error contacting cdn.nuclei.sh")
+			}
+			return
 		} else {
-			fmt.Printf("N/A\n")
+			if *noCDNPtr {
+				if !*noColorPtr {
+					color.Green("%s", ipStr)
+				} else {
+					fmt.Println(ipStr)
+				}
+				continue
+			}
+			if !*noColorPtr {
+				fmt.Printf("%s:%v%s\n", blue(ipStr), orange(result), green("N/A"))
+			} else {
+				fmt.Printf("%s:%vN/A\n", ipStr, result)
+			}
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	
+    if err := scanner.Err(); err != nil {
+        log.Println("! Error processing Input:", err)
+        log.Println("Double check your input")        
+        return
+    }
 }
