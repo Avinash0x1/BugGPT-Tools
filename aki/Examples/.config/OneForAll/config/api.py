3ae0ad9214203a804fa808338c1855f49a3d88a2
Ajam <AjamX101@gmail.com>
2023-05-17 12:37:19 +0545

    Create api.py

---
+++ aki/Examples/.config/OneForAll/config/api.py
@@ -0,0 +1,94 @@
+# API configuration
+
+# ➼ BeVigil API: https://bevigil.com/osint-api
+bevigil_api = 'your_api_key'
+
+# ➼ Binaryedge API: https://app.binaryedge.io/account/api
+#The free API is only valid for one month, and can be regenerated after expiration, and can be queried 250 times per month.
+binaryedge_api = 'your_api_key'
+
+# ➼ Bing API: https://azure.microsoft.com/en-us/free/cognitive-search/
+# May require creditcard
+bing_api_id = 'your_api_id'
+bing_api_key = 'your_api_key'
+
+# ➼ Censys API: https://censys.io/api
+censys_api_id = 'your_api_key'
+censys_api_secret = 'your_api_secret'
+
+# ➼ Chinaz API: http://api.chinaz.com/ApiDetails/Alexa
+chinaz_api = 'your_api_key'
+
+# ➼ Circl: https://www.circl.lu/services/passive-dns/
+# Allowed only to trusted partners in Luxembourg and abroad.
+# Have to submit an application: https://www.circl.lu/contact/
+circl_api_username = 'your_username'
+circl_api_password = 'your_api_key'
+
+# ➼ Cloudflare API: https://dash.cloudflare.com/profile/api-tokens
+cloudflare_api_token = 'your_api_key'
+
+# ➼ DNSDB : https://www.dnsdb.info/
+dnsdb_api_key = 'your_api_key'
+
+# ➼ Fofa : https://fofa.so/api [paid]
+fofa_api_email = 'fofa_user_email' 
+fofa_api_key = 'fofa_api_key' 
+
+# ➼ FullHunt: https://api-docs.fullhunt.io/
+fullhunt_api_key = 'your_api_key'
+
+# ➼ Github: https://github.com/settings/tokens 
+github_api_user = 'Github_Username'
+github_api_token = 'ghp_token'
+
+# ➼ Google API: https://developers.google.com/custom-search/v1/overview#search_engine_id
+#The free API can only query the first 100 results# 
+#After creating a custom search engine, you need to enable: Search the entire web on the corresponding control panel
+#https://developers.google.com/custom-search/v1/overview#api_key
+google_api_id = 'Google_API_custom_search_engine_app_api_id' 
+google_api_key = 'Google_API_custom_search_engine_app_api_key'
+
+# ➼ Hunter: https://hunter.qianxin.com/home/userInfo
+hunter_api_key = 'your_api_key'
+
+# ipv4info: http://ipv4info.com/tools/api/
+#The free API is only valid for 2 days, and can be regenerated after expiration, and can be queried 50 times a day.
+ipv4info_api_key = 'your_api_key'
+
+# ➼ PassiveDNS.cn [Deprecated]
+#passivedns_api_addr is empty by default, use http://api.passivedns.cn
+#passivedns_api_token can't be empty 
+passivedns_api_addr = ''
+passivedns_api_token = ''
+
+# ➼ PassiveTotal [RiskIQ] : https://api.passivetotal.org/api/docs/
+riskiq_api_username = 'your_email@in_lowercase'
+riskiq_api_key = 'your_api_key'
+
+# ➼ Quake 360 : After logging in to quake, you can get the key in the personal center https://quake.360.net/quake/#/personal?tab=message
+quake_api_key = 'your_api_key'
+
+# ➼ Racent: https://www.racent.com/ctlog 
+#Use Devtools (F2) >> Network capture packet to get Token
+racent_api_token = 'your_api_key'
+
+# ➼ SecurityTrails API: https://securitytrails.com/corp/api
+securitytrails_api = 'your_api_key'
+
+# ➼ Shodan: https://account.shodan.io/register
+#Free API speed limit 1 query per second, better to have developer license, or student plan or pro
+shodan_api_key = 'your_api_key'
+
+# ➼ Spyse: https://spyse.com/ [Deprecated]
+spyse_api_token = ''
+
+# ➼ ThreatBook API: https://x.threatbook.cn/nodev4/vb4/myAPI [paid]
+# After logging in to your x.threatbook.com account, you can obtain the apikey and bind the IP of the server calling the API on the "My API" page. 
+threatbook_api_key = ''
+
+# ➼ VirusTotal API: https://developers.virustotal.com/reference
+virustotal_api_key = 'your_api_key'
+
+# ➼ ZoomeEye Api: https://www.zoomeye.org/doc?channel=api
+zoomeye_api_key = 'your_api_key'
