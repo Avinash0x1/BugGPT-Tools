d4c4fc811334a51396b8e6f625a12b9294441ea2
Ajam <AjamX101@gmail.com>
2023-06-04 18:20:37 +0545

    Update GUI.sh

--- free-tiers/VPS/Google Cloud Shell/GUI.sh
+++ free-tiers/VPS/Google Cloud Shell/GUI.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 printf "Installing RDP Be Patience..." >&2
+printf "This will take ~ 10 Mins..." >&2
+printf "When Asked, Keyboard layout, Enter `1`\n" && sleep 10s
 cd $(mktemp -d)
 {
 sudo useradd -m gcprdp
 sudo adduser gcprdp sudo
-echo 'gcprdp:1337' | sudo chpasswd
+echo 'gcprdp:7331' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -23,10 +25,10 @@ sudo apt install nautilus nano -y
 sudo apt install nload
 sudo adduser gcprdp chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless `Begin` >> `Next` >> `Authorize` \n  Copy Command Of Debian Linux And Paste Down\n'
+printf "\nSetup Completed \n" >&2 ||
+printf "\nError Occured \n" >&2
+printf '\nCheck https://remotedesktop.google.com/headless `Begin` >> `Next` >> `Authorize` \n  Copy Command Of Debian Linux And Paste Down \n '
 read -p "Paste Here: " CRP
 su - gcprdp -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Username:gcprdp || Password:1337 \n'
+printf 'Check https://remotedesktop.google.com/access/ \n '
+printf 'Username:gcprdp || Password:7331 \n '
