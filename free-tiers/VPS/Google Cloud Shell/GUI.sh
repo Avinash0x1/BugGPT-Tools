5d1e1730ac2cc32def250120757d45b9c7e70d2a
Ajam <AjamX101@gmail.com>
2023-06-05 12:44:43 +0545

    Update GUI.sh

--- free-tiers/VPS/Google Cloud Shell/GUI.sh
+++ free-tiers/VPS/Google Cloud Shell/GUI.sh
@@ -6,8 +6,8 @@ export origin=$(pwd)
 sudo apt update
 sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
 # Download Chrome Remote Desktop, install it, then delete downloaded .deb package
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo dpkg -i chrome-remote-desktop* && sudo apt --fix-broken install --assume-yes && rm chrome-remote-desktop*
-sudo apt --fix-broken install --assume-yes
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo dpkg -i chrome-remote-desktop* && rm chrome-remote-desktop*
+sudo apt --fix-broken install --assume-yes 
 #Fix
 sudo apt install sassc --assume-yes
 # Remove Tmp
