4ec46cea33ca83bcd92b4ff7291d5d5893d15e70
Ajam <AjamX101@gmail.com>
2023-06-05 11:08:48 +0545

    Update GUI.sh

--- free-tiers/VPS/Google Cloud Shell/GUI.sh
+++ free-tiers/VPS/Google Cloud Shell/GUI.sh
@@ -8,6 +8,6 @@ sudo apt update && sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simple
 # Download Chrome Remote Desktop, install it, then delete downloaded .deb package
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo dpkg -i chrome-remote-desktop* && sudo apt --fix-broken install --assume-yes && rm chrome-remote-desktop*
 # Remove Tmp
-sudo rm -rf "$gui_setup" 
+sudo rm -rf "$gui_setup"
 cd "$origin"
 #EOF
