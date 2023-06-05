cf14f298abb6f8d6ac0b2033a0381ea37f9c99df
Ajam <AjamX101@gmail.com>
2023-06-05 11:40:52 +0545

    Update GUI.sh

--- free-tiers/VPS/Google Cloud Shell/GUI.sh
+++ free-tiers/VPS/Google Cloud Shell/GUI.sh
@@ -3,8 +3,10 @@
 export gui_setup=$(mktemp -d) && cd "$gui_setup"
 export origin=$(pwd)
 ##Set Deps for Core
-sudo apt update && sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
+sudo apt update
+sudo apt install dbus-x11 desktop-base firefox-esr gnome-terminal kdenlive mpv nano nautilus neofetch nload papirus-icon-theme plank sassc simplescreenrecorder xfce4 xfce4-goodies xfce4-terminal xscreensaver xvfb --assume-yes
 ##Set Deps for GUI
+sudo apt --fix-broken install
 # Download Chrome Remote Desktop, install it, then delete downloaded .deb package
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo dpkg -i chrome-remote-desktop* && sudo apt --fix-broken install --assume-yes && rm chrome-remote-desktop*
 # Remove Tmp
