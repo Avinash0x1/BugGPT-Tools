82356b947e74405b8c4bb925fecc3882db9c2fbe
Ajam <AjamX101@gmail.com>
2023-06-09 14:58:31 +0545

    Create gsync.sh

---
+++ free-tiers/VPS/Google Colab/Utils/gsync.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#Check if github is accessable: 
+if ! ping -c 2 github.com > /dev/null; then
+  echo "Github.com is Inaccessible"
+exit 1
+fi
+
+#Env variables
+export GITHUB_USER="Azathothas" #Github Username, this is CaseSensitive
+export GITHUB_EMAIL="AjamX101@gmail.com"
+export GITHUB_REPO="Azathothas/GoogleColab" #Your Gcolab Repo
+export GITHUB_TOKEN="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" # Change this to "$Token_of_Your_Github_Repo"
+export GITDIR="$HOME/GoogleColab" # The main sync directory, usually, just $HOME/REPO, DO NOT INCLUDE USERNAME
+export current_dir=$(pwd) #DO NOT CHANGE THIS
+
+#Deps
+go install -v github.com/tomnomnom/anew@latest 2>/dev/null
+
+#Set Git configs
+cd "$GITDIR"
+git config --global user.name "$GITHUB_USER"
+git config --global user.email "$GITHUB_EMAIL"
+#Mark Safe
+git config --global --add safe.directory "$GITDIR"
+#Set Repo
+git remote set-url origin "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
+#Pull changes and Reset HEAD
+cd "$HOME/$GITDIR" && git fetch --all >/dev/null 2>&1 && git reset --hard origin/main >/dev/null 2>&1 && git clean -fd >/dev/null 2>&1 && git pull --force origin main >/dev/null 2>&1
+#Set Dirs
+mkdir -p "$GITDIR/HOME/"
+#Copy Configs
+cp -r "$HOME/.config" "$GITDIR/HOME/"
+#Copy & Merge DotFiles
+cat "$HOME/.bash_hsitory" | anew -q "$GITDIR/.bash_hsitory" && cat "$GITDIR/.bash_hsitory" | anew -q "$HOME/.bash_hsitory"
+cat "$HOME/.zsh_hsitory"  | anew -q "$GITDIR/.zsh_hsitory" && cat "$GITDIR/.zsh_hsitory" | anew -q "$HOME/.zsh_hsitory"
+#Current
+history | awk '{$1=""; print substr($0,2)}' | anew -q "$HOME/.shell_history" 
+#Check if any large files:
+find "$GITDIR" -type f -size +90M | sed 's|^./||' | anew -q "$GITDIR/.gitignore"
+#Push gitignore
+git add "$GITDIR/.gitignore" && git commit -m "Ignore Large Files [gSync]" && git push --force -u origin main
+#Update README.md
+echo -e "$(uname -a) : [Last: $(date)]" > "$GITDIR/README.md"
+#Push Eeverything Else
+git add --all --verbose && git commit -m "Gcloud [Git-Push]" && git push --force -u origin main
+#EOF
