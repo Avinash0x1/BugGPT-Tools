621d2d0dcb7d0dca677a7fc5351a5c9d04fcbc72
Ajam <AjamX101@gmail.com>
2023-06-15 15:05:18 +0545

    Update debian_dns_brute_sudo_root.sh

--- free-tiers/VPS/.scripts/debian_dns_brute_sudo_root.sh
+++ free-tiers/VPS/.scripts/debian_dns_brute_sudo_root.sh
@@ -4,18 +4,18 @@
 #Deps
 #-----#
 export DEBIAN_FRONTEND=noninteractive 
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 sudo apt install curl -y
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 sudo apt install libpcap-dev -y
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 sudo apt install wget -y
 #----------------------------------------------------------------------#
 
 #----------------------------------------------------------------------#
 #Install golang
 #--------------#
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 curl -qfsSL "https://git.io/go-installer" | sudo bash
 sudo rm $HOME/go*.gz >/dev/null 2>&1
 export PATH=$HOME/go/bin:$PATH  
@@ -26,25 +26,25 @@ export PATH=$HOME/.go/bin:$PATH
 #Binaries
 #--------#
 #eget
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 curl -qfsSL "https://zyedidia.github.io/eget.sh" | sudo bash && sudo mv ./eget /usr/local/bin/eget
 #ksubdomain
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 $HOME/.go/bin/go install -v github.com/boy-hack/ksubdomain/cmd/ksubdomain@latest
 #massdns
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 sudo curl -qfsSL "https://raw.githubusercontent.com/Azathothas/BugGPT-Tools/main/free-tiers/VPS/.binaries/massdns_x86_64_ELF_LSB_Linux" -o /usr/local/bin/massdns
 sudo chmod +x /usr/local/bin/massdns
 #PureDNS
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 $HOME/.go/bin/go install -v github.com/d3mondev/puredns/v2@latest 2>/dev/null
 #----------------------------------------------------------------------#
 
 #----------------------------------------------------------------------#
 #Test
 #-----#
-clear && echo -e "==================================\n" && sudo /usr/local/bin/massdns -h
-echo -e "==================================\n" && $HOME/go/bin/goksubdomain test
-echo -e "==================================\n" && $HOME/go/bin/puredns -h
+clear && echo -e "\n==================================\n" && sudo /usr/local/bin/massdns -h
+echo -e "\n==================================\n" && $HOME/go/bin/ksubdomain test
+echo -e "\n==================================\n" && $HOME/go/bin/puredns -h
 #----------------------------------------------------------------------#
 #EOF
