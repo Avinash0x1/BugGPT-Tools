648126136105adb6424330d288d1e6a4d09488ff
Ajam <AjamX101@gmail.com>
2023-06-15 14:58:51 +0545

    Update debian_dns_brute_sudo_root.sh

--- free-tiers/VPS/.scripts/debian_dns_brute_sudo_root.sh
+++ free-tiers/VPS/.scripts/debian_dns_brute_sudo_root.sh
@@ -4,14 +4,18 @@
 #Deps
 #-----#
 export DEBIAN_FRONTEND=noninteractive 
+echo -e "==================================\n"
 sudo apt install curl -y
+echo -e "==================================\n"
 sudo apt install libpcap-dev -y
+echo -e "==================================\n"
 sudo apt install wget -y
 #----------------------------------------------------------------------#
 
 #----------------------------------------------------------------------#
 #Install golang
 #--------------#
+echo -e "==================================\n"
 curl -qfsSL "https://git.io/go-installer" | sudo bash
 sudo rm $HOME/go*.gz >/dev/null 2>&1
 export PATH=$HOME/go/bin:$PATH  
@@ -22,13 +26,17 @@ export PATH=$HOME/.go/bin:$PATH
 #Binaries
 #--------#
 #eget
+echo -e "==================================\n"
 curl -qfsSL "https://zyedidia.github.io/eget.sh" | sudo bash && sudo mv ./eget /usr/local/bin/eget
 #ksubdomain
+echo -e "==================================\n"
 $HOME/.go/bin/go install -v github.com/boy-hack/ksubdomain/cmd/ksubdomain@latest
 #massdns
+echo -e "==================================\n"
 sudo curl -qfsSL "https://raw.githubusercontent.com/Azathothas/BugGPT-Tools/main/free-tiers/VPS/.binaries/massdns_x86_64_ELF_LSB_Linux" -o /usr/local/bin/massdns
 sudo chmod +x /usr/local/bin/massdns
 #PureDNS
+echo -e "==================================\n"
 $HOME/.go/bin/go install -v github.com/d3mondev/puredns/v2@latest 2>/dev/null
 #----------------------------------------------------------------------#
 
