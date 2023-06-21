853fc0e9d8b66469786d36344fc57c34a68db4e9
Ajam <AjamX101@gmail.com>
2023-06-21 14:42:03 +0545

    Update all_get_curl_no_root.sh

--- free-tiers/VPS/.scripts/all_get_curl_no_root.sh
+++ free-tiers/VPS/.scripts/all_get_curl_no_root.sh
@@ -4,18 +4,18 @@
 #Sanity Checks
 #If curl already installed
 if command -v curl >/dev/null 2>&1; then
-   echo -e "W: curl is already installed at: $(which curl)"
+   echo -e "\nW: curl is already installed at: $(which curl)"
    echo -e "W: Why so eager to fix smth that's not broken ?\n"
-   echo -e "Further Steps: https://github.com/Azathothas/BugGPT-Tools/tree/main/free-tiers/VPS/.binaries"
+   echo -e "Further Steps: https://github.com/Azathothas/BugGPT-Tools/tree/main/free-tiers/VPS/.binaries\n"
   exit 1
 fi  
 #If invoked as root
 if [ "$EUID" -eq 0 ]; then
-   echo -e "W: This is mean to be run wthout ROOT"
+   echo -e "\nW: This is mean to be run wthout ROOT"
    echo -e "W: Running anyway may mess up all your dirs & settings"
    echo -e "W: Will also probably fail"
    echo -e "W: Hit (ctrl + c) if this was a mistake"
-   echo -e "W: Script will run anyway within 15s..."
+   echo -e "W: Script will run anyway within 15s...\n"
    sleep 15s
 fi   
 #----------------------------------------------------------------------#
